bb631aa0c6e92ef7b31ab20418bd854700eb6cca
ryo nagata <ryo.nagata.utokyo@gmail.com>
2017-02-26 22:13:13 +0900

    add users

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225144659) do
+ActiveRecord::Schema.define(version: 20170226131230) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
